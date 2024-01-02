953bd5dbcdc238771641d6f5de7f77ba5a534fd1
Prathamesh Sirdesai <sirdesai.work@gmail.com>
2024-01-02 22:41:16 +0530

    fixed bugs, added photos

--- src/data/facultyOrganiser.js
+++ src/data/facultyOrganiser.js
@@ -1,19 +1,18 @@
 const faculties =  [
     {
+        id: 1,
+        name: 'Dr. Venugopal Jalihal',
+        designation: 'Head of Depatement, BCA',
+        phno: 9513088835,
+        photo: '/Photos/image.png'
+    },
+    {
         id: 2,
         name: 'Dr. Asmita Deshpande',
         designation: 'Event Co-ordinator',
         phno: 9341116677,
         photo: '/Photos/asmita.jpg'
     },
-    {
-        id: 1,
-        name: 'Dr. Venugopal Jalihal',
-        designation: 'Head of Depatement, BCA',
-        phno: 9513088835,
-        photo: '/Photos/image.png'
-    }
-    
 ]
 
 export default faculties
